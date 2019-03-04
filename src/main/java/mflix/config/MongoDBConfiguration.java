154b33f381c081338b881e1b04853e967308be5f
Sven Dhaens <svendhaens@gmail.com>
2019-03-04 14:09:25 +0100

    exam

--- src/main/java/mflix/config/MongoDBConfiguration.java
+++ src/main/java/mflix/config/MongoDBConfiguration.java
@@ -1,8 +1,13 @@
 package mflix.config;
 
 import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.ReadConcern;
+import com.mongodb.ReadPreference;
+import com.mongodb.WriteConcern;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
+import com.mongodb.connection.SslSettings;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
@@ -24,6 +29,20 @@ public class MongoDBConfiguration {
         //DONE> Ticket: Handling Timeouts - configure the expected
         // WriteConcern `wtimeout` and `connectTimeoutMS` values
         MongoClient mongoClient = MongoClients.create(connString);
+
+        MongoClientSettings settings = MongoClientSettings.builder().applyConnectionString(connString).build();
+
+//        SslSettings sslSettings = settings.getSslSettings();
+//        ReadPreference readPreference = settings.getReadPreference();
+//        ReadConcern readConcern = settings.getReadConcern();
+//        WriteConcern writeConcern = settings.getWriteConcern();
+//
+//          EXAM: Which of the following variables contained the associated value?
+//        readPreference.toString() = "primary"
+//        sslSettings.isInvalidHostNameAllowed() = true
+//        sslSettings.isEnabled() = false
+//        readConcern.asDocument().toString() = "{ }"
+//        writeConcern.asDocument().toString() = "{ w : 1 }"
 
         return mongoClient;
     }
